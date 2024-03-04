1fd13062036ff61e326d9670055167b2d9cf9fad
17203 <17203@portalcedi.edu.mx>
2024-03-04 14:51:13 -0600

    borrar comentario

--- jugadores.cpp
+++ jugadores.cpp
@@ -75,17 +75,4 @@ void Jugador::atacar(Jugador p){
     HP = HP - p.DP;
     ultimo = p.playerName;
     }
-    /*
-        El jugador atacado(el que recibes como parámetro) pierde HP igual al ataque del jugador que ataca.
-        Existe un 20% de probabilidad de que el ataque falle y no haga daño.
-        Se debe mostrar lo que pasó en cada ataque:
-        nombre1 atacó a nombre2 e hizo 5 de daño, vida restante de nombre2 = 95
-        nombre2 atacó a nombre1 pero fallo, vida restante de nombre1 = 100
-        El jugador que atacó debe quedar guardar en la propiedad atacadoPor del jugador atacado.
-    */
-    /*
-    Nota:
-        int x = rand() % 1000; asigna un valor aleatorio entre 0 y 999 a x;
-        Puedes usar esto para la probabilidad del ataque.
-    */
 }
